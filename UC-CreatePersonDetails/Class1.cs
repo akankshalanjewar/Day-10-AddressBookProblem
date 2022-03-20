fa96e8efb159546bd7e3d80a9955bb806d2896d8
akankshalanjewar <akankshalanjewar02@gmail.com>
2022-03-20 19:26:12 +0530

    [Akanksha]: create person deatils

---
+++ UC-CreatePersonDetails/Class1.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC_CreatePersonDetails
+{
+
+    public class PersonDetail
+    {
+        public string FirstName;
+        public string LastName;
+        public string Address;
+        public string City;
+        public string State;
+        public int Zip;
+        public long PhoneNumber;
+        public string Email;
+
+        public string Result()
+        {
+            return "Name is: " + FirstName + LastName + "\nAddress: " + Address + "\nCity is " + City +
+                "\nState is " + State + "\nPostal code is: " + Zip + "\nPhone: " + PhoneNumber + "\nEmail is " + Email;
+        }
+    }
+    internal class UC_1_Program
+    {
+        public List<PersonDetail> person = new List<PersonDetail>();
+
+        public void Display()
+        {
+            Console.WriteLine("Welcome To Address Book  : ");
+            foreach (PersonDetail per in person)
+            {
+                Console.WriteLine(per.Result());
+            }
+        }
+
+        public void addPerson(PersonDetail p)
+        {
+            person.Add(p);
+            string jsonData = JsonConvert.SerializeObject(person);
+            File.WriteAllText(@"C:\Bridgelabz\Day-10-AddresssBook\UC-CreatePersonDetails\Details.json", jsonData);
+        }
+                                                                                                                                                                                       
+        public UC_1_Program()
+        {
+            string json = File.ReadAllText(@"C:\Bridgelabz\Day-10-AddresssBook\UC-CreatePersonDetails\Details.json");
+            if (json.Length > 0)
+            {                                                                                                                                                                           
+                person = JsonConvert.DeserializeObject<List<PersonDetail>>(json);
+            }
+            else
+                person = new List<PersonDetail>();
+        }
+
+    }
+}
---
+++ UC-CreatePersonDetails/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UC_CreatePersonDetails
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+                Console.WriteLine("Welcome To AddressBook Program");
+
+                UC_1_Program uC_1 = new UC_1_Program();
+                PersonDetail person = new PersonDetail();
+                person.FirstName = "Akanksha";
+                person.LastName = "Lanjewar";
+                person.Address = "Nandavan";
+                person.City = "Nagpur";
+                person.State = "Maharashtra";
+                person.Zip = 425409;
+                person.PhoneNumber = 4634678632;
+                person.Email = "akankshalanjewar@gmail.com";
+                uC_1.addPerson(person);
+                uC_1.Display();
+            
+        }
+    }
+}
